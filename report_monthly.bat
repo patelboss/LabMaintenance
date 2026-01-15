83576acb96bb0edacc3bf3557da317865325c57a
patelboss <83952959+patelboss@users.noreply.github.com>
2026-01-15 17:30:01 +0530

    Update and rename report.bat to report_monthly.bat

---
+++ report_monthly.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal
+
+set BASEDIR=C:\LabMaintenance
+set LOGFILE=%BASEDIR%\log.txt
+set PCIDFILE=%BASEDIR%\pc_id.txt
+
+if not exist "%PCIDFILE%" exit /b 1
+set /p PCID=<"%PCIDFILE%"
+
+set REPORT=%BASEDIR%\Monthly_Report_%PCID%.txt
+
+for /f "tokens=2 delims=:" %%A in ('find /c "Start:" "%LOGFILE%"') do set RUNS=%%A
+for /f "delims=" %%A in ('find "Start:" "%LOGFILE%"') do set LAST=%%A
+
+echo ============================== > "%REPORT%"
+echo PC ID: %PCID% >> "%REPORT%"
+echo Report Date: %date% >> "%REPORT%"
+echo Total Runs: %RUNS% >> "%REPORT%"
+echo Last Run: %LAST% >> "%REPORT%"
+echo Status: OK >> "%REPORT%"
+echo ============================== >> "%REPORT%"
+
+endlocal
