1ff0fba05472c1b59d845b9f99b4c567d4570c8b
patelboss <83952959+patelboss@users.noreply.github.com>
2026-03-10 14:45:55 +0530

    Update Shutdown.ps1

--- Helper/Shutdown.ps1
+++ Helper/Shutdown.ps1
@@ -1,7 +1,5 @@
 $pcs = 1..40 | ForEach-Object { "PC-$($_.ToString('00'))" }
 
-$pcs | ForEach-Object {
-    Start-Job {
-        shutdown /m \\$using:_ /s /f /t 0 /c "Lab shutdown"
-    }
-}
+Invoke-Command -ComputerName $pcs -ScriptBlock {
+    Stop-Computer -Force
+} -ThrottleLimit 10 -ErrorAction SilentlyContinue -TimeoutSec 20
