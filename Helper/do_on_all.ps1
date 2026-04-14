228a8be046520941301f3c673772f1b496b0c95b
patelboss <83952959+patelboss@users.noreply.github.com>
2026-04-14 16:46:29 +0530

    Update do_on_all.ps1

--- Helper/do_on_all.ps1
+++ Helper/do_on_all.ps1
@@ -4,3 +4,8 @@ Write-Host "Processing $pc..." -ForegroundColor Cyan;
 try{$session=New-PSSession -ComputerName $pc -ErrorAction Stop;
 #Command Here
 ;Write-Host "Successfully updated $pc" -ForegroundColor Green}catch{Write-Warning "Failed to update $pc. Error: $($_.Exception.Message)"}finally{if($session){Remove-PSSession $session}}}
+
+
+foreach($pc in $pcs){ 1..2 | %{ try{ $s=New-PSSession -CN $pc -EA Stop; 
+#Command
+; return } catch { Write-Warning "Fail $pc" } finally { if($s){Remove-PSSession $s} } } }
